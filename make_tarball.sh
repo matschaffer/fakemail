b68020604799f206dfb64da0d737347e3282d70b
lastcraft <lastcraft>
2005-04-27 19:56:37 +0000

    Tidying up the docs

--- make_tarball.sh
+++ make_tarball.sh
@@ -1,4 +1,7 @@
 chmod +x fakemail
 cd ..
-tar -zcf fakemail.tar.gz fakemail/
+tar -zcf fakemail.tar.gz fakemail/fakemail \
+                         fakemail/LICENSE \
+                         fakemail/docs/index.html \
+                         fakemail/docs/docs.css
 mv fakemail.tar.gz fakemail/
