88a57105c224a93bbbab289f03da62ec937bab47
lastcraft <lastcraft>
2005-08-29 22:01:23 +0000

    Fixing tarball builder

--- build/make_tarball.sh
+++ build/make_tarball.sh
@@ -1,12 +1,10 @@
 mkdir fakemail/
-chmod +x fakemail
 cp ../perl/fakemail fakemail/
+chmod +x fakemail/fakemail
 cp ../LICENSE fakemail/
 cp ../docs/index.html fakemail/
 cp ../docs/docs.css fakemail/
 tar -zcf fakemail.tar.gz fakemail/fakemail \
                          fakemail/LICENSE \
-                         fakemail/docs/index.html \
-                         fakemail/docs/docs.css
-mv fakemail.tar.gz fakemail/
-cd fakemail
+                         fakemail/index.html \
+                         fakemail/docs.css
